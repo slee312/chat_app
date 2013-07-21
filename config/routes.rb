594a57490379421391566fee1f752c6afd062638
Sunyan <slee312@jhu.edu>
2013-07-21 06:54:56 -0400

    Added all the static pages (yet to be filled in with content)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 ChatApp::Application.routes.draw do
   root to: 'static_pages#home'
+  get 'faq', to: 'static_pages#faq' 
+  get 'about', to: 'static_pages#about' 
+  get 'terms', to: 'static_pages#terms' 
+  get 'contact', to: 'static_pages#contact' 
 end

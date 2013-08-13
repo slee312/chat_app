2a80d5e9c7212e9dab145bf9d52f873b7a92af09
Sunyan <slee312@jhu.edu>
2013-08-13 11:14:08 -0400

    added basic map

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -21,11 +21,22 @@ function checkGeo() {
 
 function success_geo_callback(geo) {
   clearTimeout(check_time_out);
-  $('#checking_area').html("Latitude:" + geo.coords.latitude + "<br /> Longitude:" + geo.coords.longitude);
+  $('#checking_area').html("Latitude:" + geo.coords.latitude + "<br /> Longitude:" + geo.coords.longitude + "<br />");
+
+  //Add Map
+  var $mapDiv = $(document.createElement('div'));
+  $mapDiv.attr('id', 'map').style('height', '150px');
+  $('#checking_area').append($mapDiv);
+  var map = L.map('map').setView([geo.coords.latitude, geo.coords.longitude],13);
+  L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', { attribution: 'Map data &copy; OpenStreetMap; Map tiles &copy; Mapquest', maxZoom: 18, subdomains: ['otile1', 'otile2', 'otile3', 'otile4'] }).addTo(map);
+
 }
 
 function error_geo_callback(geo) {
   clearTimeout(check_time_out);
+  if (geo) {
+    alert(geo.message);
+  }
   $('#checking_area').html("<br /><br /><span class = \"error-loading-text\">There was an error in finding your location.<br / ><span class = \"button-sub-text\">If you have previously denied location access, try resetting your browser location settings.</span></span><br /><br /><button class = \"btn btn-large btn-info\" id = \"test_geo_again\" type = \"button\">Try Again</button> "); 
   $('#test_geo_again').click(checkGeoAgain);
 }

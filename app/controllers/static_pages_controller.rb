671327a9feaaccbaff7de808434f415adbe6e320
Sunyan <slee312@jhu.edu>
2013-08-18 12:09:12 -0400

    revised

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,6 @@ class StaticPagesController < ApplicationController
     #temp to test features prior to verification
     cookies.delete :in_boston
     
-
     if cookies.signed[:in_boston]
       headers["Status"] = "301 Moved Permanently"
       redirect_to(action: 'verified_home')
@@ -18,19 +17,26 @@ class StaticPagesController < ApplicationController
     app_key = "Fmjtd%7Cluub25ub2g%2C2n%3Do5-9u851f" 
     response = HTTParty.get("http://open.mapquestapi.com/geocoding/v1/reverse?key=" + app_key + "&outFormat=json&location=#{lat},#{long}")
     #parse response and determine if user is in the right city
-    city = response["results"][0]["locations"][0]["adminArea5"]
-    render text: city
+    place = response["results"][0]["locations"][0]
 
     #redirect_to(action: 'verified_home')
+    city = "Boston"
 
-=begin
-    if city.include? "Hong Kong"
-      render text: "you are in HK"
+    if place["adminArea5"].include? city || place["adminArea4"].include? city || place["adminArea3"].include? city || place["adminArea1"].include? city
       cookies.signed[:in_boston] = true
+
+      #redirect to verified page
+      headers["Status"] = "301 Moved Permanently"
+      flash[:success] = "Welcome to Clowder!"
+      redirect_to(action: 'verified_home')
     else
-      render text: "you are not in HK"
+
+      flash[:alert] = "Sorry, we do not support your location yet. Check back later!"
+      redirect_to(action: 'home')
     end 
-=end
+
+    render text: place["adminArea4"]
+
   end
 
   def verified_home

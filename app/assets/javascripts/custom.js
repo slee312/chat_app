07a7f9714d9a10aa7634f450fa8cdc052667df55
Sunyan <slee312@jhu.edu>
2013-08-18 14:08:36 -0400

    yup

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -14,11 +14,11 @@ function checkGeo() {
   $('#flash-alerts').html("");
   check_time_out = setTimeout(function() { error_geo_callback(null); }, 20000);
   if (geoPosition.init()) {
-    $('#checking_area').html("<br /><br /><span class = \"loading-text\">Checking your location...</span>");
+    $('#checking-area').html("<br /><br /><span class = \"loading-text\">Checking your location...</span>");
     geoPosition.getCurrentPosition(success_geo_callback, error_geo_callback, {enableHighAccuracy: true, timeout: 20000});
     
   } else {
-    $('#checking_area').html("<br /><br /><span class = \"error-loading-text\">Looks like your browser doesn't support geolocaton detection.<br />Try updating!</span>");
+    $('#checking-area').html("<br /><br /><span class = \"error-loading-text\">Looks like your browser doesn't support geolocaton detection.<br />Try updating!</span>");
   }
 }
 
@@ -28,13 +28,13 @@ function success_geo_callback(geo) {
   var user_long = geo.coords.longitude;
 
   //Print lat and long
-  $('#checking_area').html("<br /><h3>Looks like you're good to go!:</h3> <br /><br />");
+  $('#checking-area').html("<br /><h1 class = \"static-title\">You're good to go!</h1> <br /><br />");
 
   //Add Map
   L.Icon.Default.imagePath = "/assets";
   var $mapDiv = $(document.createElement('div'));
   $mapDiv.attr('id', 'map').css('height', '250px');
-  $('#checking_area').append($mapDiv);
+  $('#checking-area').append($mapDiv);
   var map = L.map('map').setView([user_lat, user_long],13);
   var attr_info = "Tiles Courtesy of <a href=\"http://www.mapquest.com/\" target=\"_blank\">MapQuest</a>; Data &copy; <a href=\"http://www.openstreetmap.org\" target=\"blank\">OpenStreetMap</a>";
   L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', { attribution: attr_info, maxZoom: 18, subdomains: ['otile1', 'otile2', 'otile3', 'otile4'] }).addTo(map);
@@ -46,7 +46,7 @@ function success_geo_callback(geo) {
   $('#verify_long').val(user_long);
   $('#verify_lat').val(user_lat);
 
-  createLoadBar(function() { $('#verify-location-form').submit(); }, 2000, $('#checking_area'));
+  createLoadBar(function() { $('#verify-location-form').submit(); }, 2000, $('#checking-area'));
 }
 
 //creates a loading bar with a callback function after the designated amouont of time, appended in the designated jQuery element
@@ -70,6 +70,6 @@ function error_geo_callback(geo) {
     errorMsg = "Geotracking timed out. Please try again later.";
   }
 
-  $('#checking_area').html("<br /><br /><span class = \"error-loading-text\">" + errorMsg + "</span><br /><br /><button class = \"btn btn-large btn-info\" id = \"test_geo_again\" type = \"button\">Try Again</button> "); 
+  $('#checking-area').html("<br /><br /><span class = \"error-loading-text\">" + errorMsg + "</span><br /><br /><button class = \"btn btn-large btn-info\" id = \"test_geo_again\" type = \"button\">Try Again</button> "); 
   $('#test_geo_again').click(checkGeoAgain);
 }
